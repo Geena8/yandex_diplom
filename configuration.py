0601712638d2e97f73e832848b5be72346a42e95
Geena8 <georginaadayi@gmail.com>
2024-05-19 12:19:14 +0100

    all urls

---
+++ configuration.py
@@ -0,0 +1,7 @@
+# Георгина Адайи, 16-я когорта — Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://368a0547-ad42-4163-b76f-3d4e3cea4303.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_COURIER_PATH = "/api/v1/courier"
+CREATE_ORDER_PATH = "/api/v1/orders"
+CREATE_USER_PATH = "/api/v1/users/"
+GET_ORDER_PATH = "/v1/orders/track?t="
