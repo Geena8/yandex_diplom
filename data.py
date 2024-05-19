8b54880040d8f4a7691168a09d27d8b7f4a50536
Geena8 <georginaadayi@gmail.com>
2024-05-19 12:20:18 +0100

    all bodys and headers

---
+++ data.py
@@ -0,0 +1,23 @@
+# Георгина Адайи, 16-я когорта — Финальный проект. Инженер по тестированию плюс
+headers = {
+    "Content-Type": "application/json"
+}
+courier_body = {
+    "login": "ninja",
+    "password": "1234",
+    "firstName": "saske"
+}
+
+user_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
